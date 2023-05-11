4c6468bfd7ef9801ddcd27984cb7dba6d5af27ad
Otoniel Reyes <kenliten@otonielreyes.com>
2023-05-11 17:14:41 -0400

    Updated mailer

--- src/mailers/auth.mailer.ts
+++ src/mailers/auth.mailer.ts
@@ -9,7 +9,7 @@ const {
 export const sendPasswordRecoveryEmail = (payload: { to: string, _id: string, update_token: string }) => {
 	const { to, _id, update_token } = payload;
 	return sendHtmlEmail({
-		from: '"Otoniel Reyes Galay" <otoniel@otonielreyes.com>',
+		from: '"Otoniel (kenliten) Reyes Galay" <otoniel@otonielreyes.com>',
 		to,
 		subject: "Password recovery",
 		html: `To change your password click the link bellow.<br/>
@@ -21,7 +21,7 @@ export const sendPasswordRecoveryEmail = (payload: { to: string, _id: string, up
 export const sendEmailConfirmEmail = (payload: { to: string, _id: string, confirmation_token: string }) => {
 	const { to, _id, confirmation_token } = payload;
 	return sendHtmlEmail({
-		from: '"Otoniel Reyes Galay" <otoniel@otonielreyes.com>',
+		from: '"Otoniel (kenliten) Reyes Galay" <otoniel@otonielreyes.com>',
 		to,
 		subject: "Email address confirmation",
 		html: `To confirm your email address click the link bellow.<br/>
@@ -44,10 +44,10 @@ export const sendEmailChangeConfirmEmail = (payload: { to: string, _id: string, 
 
 export const sendWelcomeEmail = (to: string) => {
 	return sendHtmlEmail({
-		from: '"Otoniel Reyes Galay" <otoniel@otonielreyes.com>',
+		from: '"Otoniel (kenliten) Reyes Galay" <otoniel@otonielreyes.com>',
 		to,
-		subject: "Welcome to Prstame",
-		html: `<h1>Welcome to Prstame</h1>
+		subject: "Welcome to AwesomeApp",
+		html: `<h1>Welcome to AwesomeApp</h1>
 		<p>It's a pleasure to have you here with us.</p>
 		<p>If you need something, just email me. I'll be pleased to help.</p>
 		`
